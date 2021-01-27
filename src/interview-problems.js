29e29a2f16c01ffc542d39b7576d0dfdefdbecf8
Balaj Khan <balajkhan07@gmail.com>
2021-01-27 16:55:19 +0500

    merge sorted arrays

--- src/interview-problems.js
+++ src/interview-problems.js
@@ -18,3 +18,40 @@ const reverseString3 = str => [...str].reverse().join('');
 console.log(reverseString1(intro));
 console.log(reverseString2(intro));
 console.log(reverseString3(intro));
+
+let arr1 = [0, 3, 4, 31];
+let arr2 = [4, 6, 30];
+
+function mergeSortedArray1(array1, array2) {
+    const mergedArray = [...array1, ...array2];
+    for (let i = 0; i < mergedArray.length; i++) {
+        for (let j = 0; j < mergedArray.length - 1 - i; j++) {
+            if (mergedArray[j] > mergedArray[j + 1])
+                [mergedArray[j], mergedArray[j + 1]] = [mergedArray[j + 1], mergedArray[j]];
+        }
+    }
+    return mergedArray;
+}
+
+function mergeSortedArray2(array1, array2) {
+    const mergedArray = [];
+    let arr1Item = array1[0];
+    let arr2Item = array2[0];
+    let i = 1;
+    let j = 1;
+    while (arr1Item || arr2Item) {
+        if (!arr2Item || arr1Item < arr2Item) {
+            mergedArray.push(arr1Item);
+            arr1Item = array1[i];
+            i++;
+        } else {
+            mergedArray.push(arr2Item);
+            arr2Item = array2[j];
+            j++;
+        }
+    }
+    return mergedArray;
+}
+
+console.log(mergeSortedArray1(arr1, arr2));
+console.log(mergeSortedArray2(arr1, arr2));
